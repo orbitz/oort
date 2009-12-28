594147ae70ad63ba363158cb3ebe32ba60a8658b
Ort <orbitz@blong.(none)>
2009-12-28 01:47:54 -0500

    Removed reverse call that was in there for debugging

--- src/irc_lib.erl
+++ src/irc_lib.erl
@@ -65,7 +65,7 @@ init([Client]) ->
     Dict = dict:from_list([
                            {nick, Client#irc_client_info.nick},
                            {realname, Client#irc_client_info.realname},
-                           {servers, lists:reverse(Client#irc_client_info.servers)},
+                           {servers, Client#irc_client_info.servers},
                            {password, Client#irc_client_info.password}]),
     {ok, #state{client=Dict, sock=undefined, state=disconn, cmd_queue=queue:new(), cmd_curr=undefined}}.
 
